6d3a3891179f143d8103b1a26d37fd61ed0094d8
Víctor <lembrant32@gmail.com>
2022-03-28 10:35:44 +0200

    csrf

--- BoogeyBookAPP/admin.py
+++ BoogeyBookAPP/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from BoogeyBookAPP.models import Reader, Author, Genre, Book
+
+admin.site.register(Reader)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Book)
 
 # Register your models here.

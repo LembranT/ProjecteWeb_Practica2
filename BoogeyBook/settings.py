955eabe1d7532245297ff62c301758b30cc74460
Víctor <lembrant32@gmail.com>
2022-03-28 10:21:18 +0200

    csrf

--- BoogeyBook/settings.py
+++ BoogeyBook/settings.py
@@ -25,7 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['*']
+
 
 
 # Application definition
